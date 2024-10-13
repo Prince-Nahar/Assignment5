eecf2b7369b4c77115ed052cb9c4eb4b0be4c41a
tuk86214 <tuk86214@temple.edu>
2024-10-13 15:24:28 -0400

    Added the 10 images as drawables and added a songAdapter class.

--- app/src/main/java/edu/temple/dualactivitybase/SelectionActivity.kt
+++ app/src/main/java/edu/temple/dualactivitybase/SelectionActivity.kt
@@ -6,11 +6,27 @@ import androidx.recyclerview.widget.RecyclerView
 
 class SelectionActivity : AppCompatActivity() {
 
+
     val recyclerView : RecyclerView by lazy {
         findViewById(R.id.recyclerView)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val images = arrayOf(
+            R.drawable.first,
+            R.drawable.second,
+            R.drawable.third,
+            R.drawable.fourth,
+            R.drawable.fifth,
+            R.drawable.sixth,
+            R.drawable.seventh,
+            R.drawable.eighth,
+            R.drawable.nineth,
+            R.drawable.tenth
+        )
+
     }
 }
---
+++ app/src/main/java/edu/temple/dualactivitybase/SongAdapter.kt
@@ -0,0 +1,4 @@
+package edu.temple.dualactivitybase
+
+class SongAdapter {
+}
