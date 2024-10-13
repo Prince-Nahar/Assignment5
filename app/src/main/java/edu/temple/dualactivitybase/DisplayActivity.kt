87d6f924701ca3ec70544902085e968414aac8cb
tuk86214 <tuk86214@temple.edu>
2024-10-13 17:44:45 -0400

    Display selected song details in DisplayActivity and Updated layouts for DisplayActivity and SelectionActivity

--- app/src/main/java/edu/temple/dualactivitybase/DisplayActivity.kt
+++ app/src/main/java/edu/temple/dualactivitybase/DisplayActivity.kt
@@ -1,14 +1,38 @@
 package edu.temple.dualactivitybase
 
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class DisplayActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
 
+
+        val songImageView: ImageView = findViewById(R.id.songImage)
+        val songTitleTextView: TextView = findViewById(R.id.songTitle)
+        val closeButton: Button = findViewById(R.id.closeButton)
+
+
+        val song = intent.getParcelableExtra<Song>("song")
+
+
+        if (song != null) {
+            songImageView.setImageResource(song.imageResource)
+            songTitleTextView.text = song.title
+        }
+
+
+        // Close button to finish the activity
+        closeButton.setOnClickListener {
+            finish()
+        }
     }
 }
